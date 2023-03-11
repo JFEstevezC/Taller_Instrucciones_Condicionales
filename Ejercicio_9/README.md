15bf573debdc23b58cb5fc4d717ebb181e735832
JFEstevezC <jose2224646@correo.uis.edu.co>
2023-03-11 08:26:35 -0500

    Ejercicio 9

---
+++ Ejercicio_9/README.md
@@ -0,0 +1,6 @@
+# EJERCICIO 9
+## ANÁLISIS
+Determinar si un triángulo es obtuso, recto o agudo, dadas las longitudes de sus tres lados.
+## DISEÑO
+### DIAGRAMA DE FLUJO
+![Diagrama de Flujo](diagrama.png "Diagrama de flujo")
---
+++ Ejercicio_9/tipo_triangulo.py
@@ -0,0 +1,13 @@
+import math
+print("TIPO DE TRIÁNGULO")
+a = int(input("Ingrese el lado a del triángulo: "))
+b = int(input("Ingrese el lado b del triángulo: "))
+c = int(input("Ingrese el lado c del triángulo: "))
+
+r = a^2 + b^2
+if r == c^2:
+    print("El triángulo es rectángulo")
+elif r < c^2:
+    print("El triángulo es agudo")
+else:
+    print("El triángulo es obtuso")
