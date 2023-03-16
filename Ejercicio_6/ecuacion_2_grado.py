cbf291a49ce876c1eedb6cd299322fcb31cdba5d
JFEstevezC <jose2224646@correo.uis.edu.co>
2023-03-16 09:25:02 -0500

    Ejercicio 6

--- Ejercicio_6/ecuacion_2_grado.py
+++ Ejercicio_6/ecuacion_2_grado.py
@@ -1,27 +1,20 @@
-import math
-print("ECUACIÓN CUADRÁTICA")
-print("La ecuación cuadrática es de la forma ax^2 + bx + c = 0")
 
-a = int(input("Ingrese el coeficiente de ax^2: "))
-b = int(input("Ingrese el coeficiente de bx: "))
-c = int(input("Ingrese el coeficiente de c: "))
-
-r = complex
-r1 = complex
-r2 = complex
-
-e = (b^2)-(4*a*c)
-
-r = math.sqrt(e)
-
-r1 = (-b + r)/2*a
-r1 = (-b - r)/2*a
-
-print (r1)
-print (r2)
+print("-----------------------------------------")
+print("---RESOLVER-LA-ECUACION-CUADRATICA-------") 
+print("-----------------------------------------")
 
 
+from math import sqrt
+A = int(input("Ingrese el valor de a: "))
+B = int(input("Ingrese el valor de b: "))
+C = int(input("Ingrese el valor de c: "))
 
 
-
-# Calcular e imprimir las raíces de la ecuación de segundo grado de coeficientes reales. El programa debe tener en cuenta los diferentes casos que puedan surgir: la existencia de dos raíces reales distintas, de dos raíces reales iguales y de dos raíces complejas.
+if ((B**2)-4*A*C) < 0:
+  print("La solución de la ecuación es con números complejos")
+else:
+  x1 = (-B+sqrt(B**2-(4*A*C)))/(2*A)
+  x2 = (-B-sqrt(B**2-(4*A*C)))/(2*A)
+  print("Las soluciones de la ecuación son:")
+  print("x1= ",x1)
+  print("x2= ",x2)
